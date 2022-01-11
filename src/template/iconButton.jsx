b9160c7cde0c6f84dab661971391dae3198fbab6
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2022-01-11 10:54:20 -0300

    Renderização Condicional (If)

--- src/template/iconButton.jsx
+++ src/template/iconButton.jsx
@@ -1,14 +1,13 @@
 import React from "react";
+import If from './if'
 
-export default props => {
-    if(props.hide){
-        return null
-    } else {
-        return (
-        <button className={'btn btn-'+ props.style}
-         onClick={props.onClick}>
-             <i className={'fa fa-'+ props.icon}></i>
-         </button>
-        )
-    }
-}
+export default props => (
+    <If teste={!props.hide}> {/*Se o teste não esta escondido automaticamente ele mostra e se estiver escondido ela pula renderizaçap*/}
+     <button className={'btn btn-'+ props.style}
+        onClick={props.onClick}>
+         <i className={'fa fa-'+ props.icon}></i>
+   </button>
+      </If>
+
+)
+
