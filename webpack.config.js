25985f0560af5740824ac3cf5673358d20092eee
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2022-01-11 08:28:57 -0300

    Configuração e Instalação FRONTEND

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: __dirname +'/public',
+        filename: './app.js'
+
+    },
+
+    devServer: {
+        port: 8080,
+        contentBase: '.public',
+    },
+
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
+        alias: {
+            modules: __dirname + '/node_modules'
+        }
+    }
+
+}
