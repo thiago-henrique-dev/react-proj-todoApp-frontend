24bd5fb32ba809a39c534145b7243cb645a56a68
thiago-henrique-dev <thiagohenriquedev@gmail.com>
2022-01-11 09:51:45 -0300

    Componente PageHeader

--- src/template/menu.jsx
+++ src/template/menu.jsx
@@ -1,23 +1,17 @@
-import React from 'react'
+import React from "react";
 
-export default props => (
-    <nav className='navbar navbar-inverse bg-inverse'>
-        <div className='container'>
-            <div className='navbar-header'>
-                <a className='navbar-brand' href='#'>
-                    <i className='fa fa-calendar-check-o'></i> TodoApp
-                </a>
-            </div>
-
-            <div id='navbar' className='navbar-collapse collapse'>
-                <ul class='nav navbar-nav'>
-                    <li><a href="#/todos">Tarefas</a></li>
-                    <li><a href="#/about">Sobre</a></li>
-                </ul>
+export default prosp => (
+    <nav className="navbar navbar-inverse bg-inverse">
+        <div className="container">
+            <div className="navbar-header">
+                <a href="#/todos" className="navbar-brand"><i className="fa fa-calendar-check-o"></i> TodoApp</a> 
             </div>
         </div>
-
+        <div id='navbar' className="navbar-collapse collapse">
+            <ul className="nav navbar-nav">
+                <li><a href="#/todos">Tarefa</a></li>
+                <li><a href="#/about">Sobre</a></li>
+            </ul>
+        </div>
     </nav>
-
-
 )
